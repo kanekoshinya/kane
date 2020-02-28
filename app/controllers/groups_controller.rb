37f4123abd1520003a02017c5df0ecf04f48a426
kanekoshinya <knksny777@icloud.com>
2020-02-28 17:58:13 +0900

    検索機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
     @group = Group.where(category_id:params[:category_id]).paginate(page: params[:page],per_page: 5)
     @q = Group.ransack(params[:q])
     @groups = @q.result(distinct: true).paginate(page: params[:page],per_page: 5)
+    @check = params[:q]
   end
 
   def new
@@ -14,6 +15,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @groups = Group.where(group_id:params[:group_id])
     if @group.save
       redirect_to root_path
     else
